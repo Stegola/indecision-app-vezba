69919c9ce5afb480bb384a35ec2bbb6f57979e13
stegola <sgolubovic93@gmail.com>
2018-03-24 15:21:35 +0100

    init commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>All Expenses</NavLink><br />
-        <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink><br />
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
+const Header = (props) => (
+    <div className="header">
+        <div className="container">
+            <h1 className="header__title">{props.title}</h1>
+            {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+        </div>
+    </div>
 );
+
+Header.defaultProps = {
+    title: 'Indecision'
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routes/AppRouter';
-import cofigureStore from './store/configureStore';
-import { addExpense, removeExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.sass';
-import 'react-dates/lib/css/_datepicker.css';
 
-const store = cofigureStore();
-const unsubscribe = store.subscribe(() => {
-    // console.log(visibleExpenses);
-});
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-const jsx = (
-    <Provider store={store}> 
-        <AppRouter />
-    </Provider>
-);
-
-ReactDOM.render(
-    jsx,
-    document.getElementById('root')
-);
+ReactDOM.render(<IndecisionApp />, window.document.getElementById("root"));
