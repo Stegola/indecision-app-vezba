b5446badf1f16eb85d6b3b7381ac2bda14e9ca27
stegola <sgolubovic93@gmail.com>
2018-03-24 17:32:42 +0100

    zezanje

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -11,6 +11,7 @@ const OptionModal = (props) => (
         className="modal"
     >
         <h3 className="modal__title">Selected Option</h3>
+        <p>Anka PRVO mora da uci, a posle moze</p><br/>
         {props.selectedOption && <p className="modal__body">{props.selectedOption === "Clubbing" ? "Anka ne moze da Clubbinuje!" : props.selectedOption}</p>}
         <button className="button" onClick={props.handleClearOption} >Okey</button>
     </Modal>
